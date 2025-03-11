98e93b9070ff41a9a0e9da42caf90f7331d42214
RudraRajbanshi <rudra.raj.716@gmail.com>
2025-03-11 11:36:46 +0545

    Test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
                         <img class="desktop-logo h-24 w-auto" src="{{ asset('assets/images/logo.png') }}"
                             alt="logo">
                     </div>
-                    <p>This is for test</p>
+                    <p>This is for test123</p>
 
                     @if (session('error'))
                         <div
