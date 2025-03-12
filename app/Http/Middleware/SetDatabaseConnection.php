86798c810a9ef6570eb257abd2adad1e7db58c10
RudraRajbanshi <rudra.raj.716@gmail.com>
2025-03-12 14:14:01 +0545

    DB

--- app/Http/Middleware/SetDatabaseConnection.php
+++ app/Http/Middleware/SetDatabaseConnection.php
@@ -19,60 +19,62 @@ class SetDatabaseConnection
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // Extract subdomain from the host
-        $host = $request->getHost();
-        $hostParts = explode('.', $host) ?? [];
-        $subdomain = $hostParts[0] ?? ''; // Fallback to 'default' if no subdomain
+        if (config('app.env') !== 'local') {
+            // Extract subdomain from the host
+            $host = $request->getHost();
+            $hostParts = explode('.', $host) ?? [];
+            $subdomain = $hostParts[0] ?? ''; // Fallback to 'default' if no subdomain
 
-        $cacheKey = 'db_config_' . $subdomain;
+            $cacheKey = 'db_config_' . $subdomain;
 
-        $dbConfig = Cache::remember($cacheKey, 60, function () use ($subdomain) {
-            $response = Http::get('https://attendance.paradiseit.com.np/api/getdatabase', [
-                'subdomain' => $subdomain,
-            ]);
+            $dbConfig = Cache::remember($cacheKey, 60, function () use ($subdomain) {
+                $response = Http::get('https://attendance.paradiseit.com.np/api/getdatabase', [
+                    'subdomain' => $subdomain,
+                ]);
 
-            if ($response->successful()) {
-                return $response->json();
-            }
+                if ($response->successful()) {
+                    return $response->json();
+                }
 
-            Log::error('Failed to fetch database configuration for subdomain: ' . $subdomain);
-            return null;
-        });
-        // dd($dbConfig);
+                Log::error('Failed to fetch database configuration for subdomain: ' . $subdomain);
+                return null;
+            });
+            // dd($dbConfig);
 
-        if (isset($dbConfig['data']) && isset($dbConfig['data']['database'], $dbConfig['data']['username'], $dbConfig['data']['password'])) {
-            Config::set('database.connections.mysql', [
-                'driver' => 'mysql',
-                'host' => env('DB_HOST', '127.0.0.1'),
-                'port' => env('DB_PORT', '3306'),
-                'database' => $dbConfig['data']['database'],
-                'username' => $dbConfig['data']['username'],
-                'password' => $dbConfig['data']['password'],
-                'charset' => 'utf8mb4',
-                'collation' => 'utf8mb4_unicode_ci',
-                'prefix' => '',
-                'strict' => true,
-                'engine' => null,
-            ]);
+            if (isset($dbConfig['data']) && isset($dbConfig['data']['database'], $dbConfig['data']['username'], $dbConfig['data']['password'])) {
+                Config::set('database.connections.mysql', [
+                    'driver' => 'mysql',
+                    'host' => env('DB_HOST', '127.0.0.1'),
+                    'port' => env('DB_PORT', '3306'),
+                    'database' => $dbConfig['data']['database'],
+                    'username' => $dbConfig['data']['username'],
+                    'password' => $dbConfig['data']['password'],
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_unicode_ci',
+                    'prefix' => '',
+                    'strict' => true,
+                    'engine' => null,
+                ]);
 
-            DB::purge('mysql');
-            DB::reconnect('mysql');
+                DB::purge('mysql');
+                DB::reconnect('mysql');
 
-            Config::set('database.default', 'mysql');
-            try {
-                DB::connection()->getPdo();
-            } catch (\Exception $e) {
+                Config::set('database.default', 'mysql');
+                try {
+                    DB::connection()->getPdo();
+                } catch (\Exception $e) {
+                    return response()->json(
+                        'Database Authentication Failed',
+                        500
+                    );
+                }
+            } else {
                 return response()->json(
                     'Database Authentication Failed',
                     500
                 );
+                // return redirect('https://paradiseit.com.np')->setStatusCode(500, 'Database Connection Failed');
             }
-        } else {
-            return response()->json(
-                'Database Authentication Failed',
-                500
-            );
-            // return redirect('https://paradiseit.com.np')->setStatusCode(500, 'Database Connection Failed');
         }
 
         return $next($request);
